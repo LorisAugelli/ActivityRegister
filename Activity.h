e5fd5565f623d93af6d1f3e17c17a885e608562b
Z3X0S <11964416+Z3X0S@users.noreply.github.com>
2024-07-11 17:20:34 +0200

    Inizio dello sviluppo

---
+++ Activity.h
@@ -0,0 +1,22 @@
+//
+// Created by Z3X0S on 11/07/24.
+//
+
+#ifndef ACTIVITYREGISTER_ACTIVITY_H
+#define ACTIVITYREGISTER_ACTIVITY_H
+
+#include <string>
+using namespace std;
+
+class Activity {
+public:
+    Activity(){};
+
+private:
+    string activityName;
+    string description;
+
+
+
+};
+#endif //ACTIVITYREGISTER_ACTIVITY_H
